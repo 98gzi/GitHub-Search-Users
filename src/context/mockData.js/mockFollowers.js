01103865d699b9a7c83b2b81bb78abdc401f381b
Eduards Gzibovskis <gzibovskis1998@gmail.com>
2022-01-02 16:03:00 +0000

    first commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    login: "mhamwala",
+    id: 25488126,
+    node_id: "MDQ6VXNlcjI1NDg4MTI2",
+    avatar_url: "https://avatars.githubusercontent.com/u/25488126?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/mhamwala",
+    html_url: "https://github.com/mhamwala",
+    followers_url: "https://api.github.com/users/mhamwala/followers",
+    following_url:
+      "https://api.github.com/users/mhamwala/following{/other_user}",
+    gists_url: "https://api.github.com/users/mhamwala/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/mhamwala/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/mhamwala/subscriptions",
+    organizations_url: "https://api.github.com/users/mhamwala/orgs",
+    repos_url: "https://api.github.com/users/mhamwala/repos",
+    events_url: "https://api.github.com/users/mhamwala/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/mhamwala/received_events",
+    type: "User",
+    site_admin: false,
+  },
+];
---
+++ src/context/mockData.js/mockRepos.js
@@ -0,0 +1,120 @@
+export default [
+  {
+    id: 437810458,
+    node_id: "R_kgDOGhh1Gg",
+    name: "PDF-to-PNG",
+    full_name: "98gzi/PDF-to-PNG",
+    private: false,
+    owner: {
+      login: "98gzi",
+      id: 48183390,
+      node_id: "MDQ6VXNlcjQ4MTgzMzkw",
+      avatar_url: "https://avatars.githubusercontent.com/u/48183390?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/98gzi",
+      html_url: "https://github.com/98gzi",
+      followers_url: "https://api.github.com/users/98gzi/followers",
+      following_url:
+        "https://api.github.com/users/98gzi/following{/other_user}",
+      gists_url: "https://api.github.com/users/98gzi/gists{/gist_id}",
+      starred_url: "https://api.github.com/users/98gzi/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/98gzi/subscriptions",
+      organizations_url: "https://api.github.com/users/98gzi/orgs",
+      repos_url: "https://api.github.com/users/98gzi/repos",
+      events_url: "https://api.github.com/users/98gzi/events{/privacy}",
+      received_events_url: "https://api.github.com/users/98gzi/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    html_url: "https://github.com/98gzi/PDF-to-PNG",
+    description: null,
+    fork: false,
+    url: "https://api.github.com/repos/98gzi/PDF-to-PNG",
+    forks_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/forks",
+    keys_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/teams",
+    hooks_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/issues/events{/number}",
+    events_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/events",
+    assignees_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/branches{/branch}",
+    tags_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/tags",
+    blobs_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/git/refs{/sha}",
+    trees_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/statuses/{sha}",
+    languages_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/languages",
+    stargazers_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/subscription",
+    commits_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/merges",
+    archive_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/{archive_format}{/ref}",
+    downloads_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/downloads",
+    issues_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/issues{/number}",
+    pulls_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/notifications{?since,all,participating}",
+    labels_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/labels{/name}",
+    releases_url: "https://api.github.com/repos/98gzi/PDF-to-PNG/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/98gzi/PDF-to-PNG/deployments",
+    created_at: "2021-12-13T09:26:54Z",
+    updated_at: "2021-12-13T09:28:10Z",
+    pushed_at: "2021-12-13T09:28:05Z",
+    git_url: "git://github.com/98gzi/PDF-to-PNG.git",
+    ssh_url: "git@github.com:98gzi/PDF-to-PNG.git",
+    clone_url: "https://github.com/98gzi/PDF-to-PNG.git",
+    svn_url: "https://github.com/98gzi/PDF-to-PNG",
+    homepage: null,
+    size: 11985,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: "Roff",
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    topics: [],
+    visibility: "public",
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: "main",
+  },
+];
